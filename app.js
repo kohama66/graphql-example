55bb3fa77dc3557ede14cf76d7a5c70fcc438eea
kohama66 <54892265+kohama66@users.noreply.github.com>
2022-09-12 01:06:57 +0900

    add users

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
+const { users } = require("./user");
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  type User {
+    id: Int
+    name: String
+    age: Int
+    created_date: String
+  }
   type Query {
     hello: String
+    users: [User]
   }
 `;
 
@@ -12,15 +20,16 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => "Hello world!",
+    users: () => users,
   },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
-server.start().then(res => {
+server.start().then((res) => {
   server.applyMiddleware({ app });
   app.listen({ port: 4000 }, () =>
-      console.log('Now browse to http://localhost:4000' + server.graphqlPath)
-  )
- })
+    console.log("Now browse to http://localhost:4000" + server.graphqlPath)
+  );
+});
